2a93752fd0aaa58e2323923a2d6fa06758cd3b83
Andry Giorgio Farrugia <andrygiorgiofarrugia@gmail.com>
2024-12-05 21:41:58 +0100

    Add Register and Login Connexion

--- Winnie/frontend/app/src/App.vue
+++ Winnie/frontend/app/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <ButtonAuth />
     <router-view />
   </div>
 </template>
 
 <script>
 
+import ButtonAuth from '@/components/ButtonAuth.vue'
+
 export default {
   name: 'App',
+  components: {
+    ButtonAuth
+  }
 };
 </script>
